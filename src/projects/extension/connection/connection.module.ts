2011f71279230cd85e3aaf4a5572ade4f06b57cc
Ralf Hannuschka <r.hannuschka@gmail.com>
2020-10-18 21:50:12 +0200

    fix: fixed bug we connected twice if we added a qix directory to an empty workspace
    closes #602

--- src/projects/extension/connection/connection.module.ts
+++ src/projects/extension/connection/connection.module.ts
@@ -2,13 +2,15 @@ import { ConnectionStorage, ExtensionContext } from "@data/tokens";
 import * as vscode from "vscode";
 import { FileStorage, MemoryStorage } from "@core/storage";
 import { SettingsOpenCommand } from "@settings";
-import { LoggerFactory } from "@vsqlik/logger";
+import { LoggerFactory, VsQlikLogger } from "@vsqlik/logger";
 import { container, inject, singleton } from "tsyringe";
 import { COMMANDS, VsQlikLoggerConnection } from "./api";
 import { AddConnectionCommand, ServerConnectCommand, ServerDisconnectCommand, RemoveConnectionCommand } from "./commands";
 
 @singleton()
 export class ConnectionModule {
+
+    private logger: VsQlikLogger;
 
     constructor(
         @inject(ExtensionContext) private extensionContext: vscode.ExtensionContext
@@ -21,6 +23,23 @@ export class ConnectionModule {
         this.registerCommands();
         this.createConnectionStorage();
         this.registerLogger();
+    }
+
+    public initialize(): void {
+        this.logger = container.resolve(VsQlikLoggerConnection);
+
+        /**
+         * register existing workspace folders (for example close and reopen editor)
+         * fs or connect good question
+         */
+        vscode.workspace.workspaceFolders?.forEach((folder) => {
+            if (folder.uri.scheme === 'qix') {
+                this.logger.info(`found existing qix workspace folder ${folder.name}`);
+                vscode.commands.executeCommand(COMMANDS.CONNECT, folder);
+            }
+        });
+
+        this.registerOnWorkspaceChanges();
     }
 
     private registerLogger() {
@@ -50,5 +69,37 @@ export class ConnectionModule {
 
         /** register connection storage */
         container.register(ConnectionStorage, {useValue: storage});
+    }
+
+    /**
+     * register workspace folder events to get notified about our active connections
+     */
+    private registerOnWorkspaceChanges() {
+
+        vscode.workspace.onDidChangeWorkspaceFolders((event) => {
+            event.added.forEach((folder) => {
+                if(folder.uri.scheme === 'qix') {
+                    /**
+                     * if we handle an empty workspace and added 1 folder (qix) the extension host
+                     * will restart soon and then find a qix fs and try to connect to it.
+                     *
+                     * So we ignore this if we have only 1 workspace folder to avoid 1 connection
+                     * which is not required.
+                     */
+                    this.logger.info(`add qix workspace folder ${folder.name}`);
+                    const qixWorkspaceFolders = vscode.workspace.workspaceFolders?.filter((folder) => folder.uri.scheme === 'qix') ?? [];
+                    if (qixWorkspaceFolders.length > 1) {
+                        vscode.commands.executeCommand(COMMANDS.CONNECT, folder);
+                    }
+                }
+            });
+
+            event.removed.forEach((folder) => {
+                if(folder.uri.scheme === 'qix') {
+                    this.logger.info(`remove qix workspace folder ${folder.name}`);
+                    vscode.commands.executeCommand(COMMANDS.DISCONNECT, folder);
+                }
+            });
+        });
     }
 }
